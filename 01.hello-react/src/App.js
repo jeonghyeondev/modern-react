1df4a5e09bf0d17aa1e399e5d60e182e544656da
kkkkkjjjjjh <rlwjgu12@gmail.com>
2020-03-11 01:20:24 +0900

    이벤트 핸들링 EventPractice.js add 128p

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import MyComponent from './MyComponent';
-import Counter from './Counter';
+// import MyComponent from './Component/MyComponent';
+// import Counter from './Counter';
+// import Say from './Component/Say';
+import EventPractice from './Component/EventPractice';
 
 const App = () => {
-  return <Counter />;
+  return <EventPractice />;
 };
 
 export default App;
---
+++ 01.hello-react/src/Component/Say.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+  const [message, setMessage] = useState('');
+  const onClickEnter = () => setMessage('안녕하세요!');
+  const onClickLeave = () => setMessage('안녕히 가세요!');
+
+  const [color, setColor] = useState('black');
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        빨간색
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        파란색
+      </button>
+      <button style={{ color: 'purple' }} onClcik={() => setColor('purple')}>
+        보라색
+      </button>
+    </div>
+  );
+};
+
+export default Say;
