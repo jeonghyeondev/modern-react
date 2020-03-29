3d55b63ee9b98a7f336984908690daf82ac2f114
kkkkkjjjjjh <rlwjgu12@gmail.com>
2020-03-29 23:18:16 +0900

    06.router-tutorial add, 프로필까지

---
+++ 06.router-tutorial/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Profiles from './Profiles';
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profiles" component={Profiles} />
+    </div>
+  );
+};
+
+export default App;
---
+++ 06.router-tutorial/src/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const data = {
+  kkkkkjjjjh: {
+    name: 'kjh',
+    description: '하하하하하하,,,,,,,,, 여긴 어디 난 누구,,',
+  },
+  jay: {
+    name: 'jay',
+    description: '내일 출근이다~~~~~ ',
+  },
+};
+const Profile = ({ match }) => {
+  const { username } = match.params;
+  const profile = data[username];
+  if (!profile) {
+    return <div>존재하지 않는 사용자이니다.</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username}({profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+};
+
+export default Profile;
