6dddaabeb00cc976217f566d5a9751618dd34fdd
kkkkkjjjjjh <rlwjgu12@gmail.com>
2020-04-23 16:18:35 +0900

    news api categories 적용

--- 07.news-viewer/src/components/NewsItem.js
+++ 07.news-viewer/src/components/NewsItem.js
@@ -3,18 +3,47 @@ import styled from 'styled-components';
 /**
  * 각 뉴스 정보를 보여주는 컴포넌트
  */
+const NewsItemBlock = styled.div`
+  display: flex;
 
-const NewsItem = ({ props }) => {
-  const { title, description, url, urlToImage } = props;
+  .thumbnail {
+    margin-right: 1rem;
+    img {
+      display: block;
+      width: 160px;
+      height: 100px;
+      object-fit: cover;
+    }
+  }
+  .contents {
+    h2 {
+      margin: 0;
+      a {
+        color: black;
+      }
+    }
+    p {
+      margin: 0;
+      line-height: 1.5;
+      margin-top: 0.5rem;
+      white-space: normal;
+    }
+  }
+  & + & {
+    margin-top: 3rem;
+  }
+`;
+const NewsItem = ({ article }) => {
+  const { title, description, url, urlToImage } = article;
   return (
     <NewsItemBlock>
-      {urlToImage&&{
+      {urlToImage && (
         <div className="thumbnail">
           <a href={url} target="_blank" rel="noopener noreferrer">
-            <img src={urlToImage} alt={title} />
+            <img src={urlToImage} alt="thumbnail" />
           </a>
         </div>
-      }}
+      )}
       <div className="contents">
         <h2>
           <a href={url} target="_blank" rel="noopener noreferrer">
